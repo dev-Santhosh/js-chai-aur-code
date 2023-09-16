074f6b3ef7a65b39b8a998afa6bb3c3010594ea7
dev-Santhosh <130728015+dev-Santhosh@users.noreply.github.com>
2023-09-16 10:45:09 +0000

    conversion operations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,53 @@
+let score = 33
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let score1= "23"
+console.log(score1);
+console.log(typeof score1);
+
+let score2="23san"
+console.log(typeof score2);
+let valueIn = Number(score2)
+console.log(valueIn);
+console.log(typeof valueIn);
+
+let score3= null
+let valueIn2 = Number(score3)
+console.log(typeof score3)
+console.log(typeof valueIn2);
+console.log(valueIn2);
+
+let score4 = undefined
+let valueIn3 = Number(score4)
+console.log(typeof score4);
+console.log(typeof valueIn3);
+console.log(valueIn3);
+
+let score5 = "Santhosh"
+let valueIn4 = Number(score5)
+console.log(typeof score5);
+console.log(typeof valueIn4);
+console.log(valueIn4)
+
+let score6= true
+let valueIn5 = Number(score6)
+console.log(typeof score6)
+console.log(typeof valueIn5);
+console.log(valueIn5);
+
+let score7 = 1
+let valueIn6 = Boolean(score7)
+console.log(typeof score7);
+console.log(typeof valueIn6);
+console.log(valueIn6);
+// 1 => true
+// 0=> false
+// ""=> false
+// "Santhosh" => true
+
+let score8 = 23
+let valueIn7 = String(score8)
+console.log(valueIn7)
+console.log(typeof valueIn7)
