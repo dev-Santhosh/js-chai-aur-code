bfa403a5e64711a27d19dbe03fc9c1e41ec523d6
dev-Santhosh <130728015+dev-Santhosh@users.noreply.github.com>
2023-09-16 09:04:43 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 2323525;
+let accountEmail = "santhosh@gmail.com";
+var accountPassword ="253452";
+accountCity ="Jaipur";
+console.log(accountId);
+let accountState;
+// accountId=45656546; not possible
+
+accountEmail= "santhosg547@gmail.com";
+accountPassword = "965989";
+accountCity = "Hyderabad";
+
+console.table([accountEmail, accountPassword, accountCity,accountState]);
+
+/*Do not use var because of functional and block scope*/ 
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat the code as newer version
+// alert(3+3) we are using nodejs, not browser
+console.log(3+3)
+console.log("Santhosh")
+let name ="Santhosh"
+let age = 22
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => false & true
+// null => standalone value
+// undefined 
+// symbol => uniqueness
+// object
+
+console.log(typeof "santhosh")
+console.log(typeof null)
+console.log(typeof undefined)
