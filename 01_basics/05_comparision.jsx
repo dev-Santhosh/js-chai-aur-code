185775c6c07affc223a28541f2a0cee5a90935be
dev-Santhosh <130728015+dev-Santhosh@users.noreply.github.com>
2023-09-16 14:24:56 +0000

    all commit

---
+++ 01_basics/05_comparision.jsx
@@ -0,0 +1,14 @@
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2!=1);
+console.log(2==1);
+// Avoid comaprision of null and undefined
+console.log(null==0)
+console.log(null<0)
+console.log(null>0)
+console.log(null>=0);
+// "==" and "==="
+console.log("2"==2);
+console.log("2"===2);
