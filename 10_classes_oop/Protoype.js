822c52e7a52d4d5955f27ee47b4887ba27800fe0
dev-Santhosh <130728015+dev-Santhosh@users.noreply.github.com>
2023-10-03 14:47:51 +0000

    prototype commit

--- 10_classes_oop/Protoype.js
+++ 10_classes_oop/Protoype.js
@@ -1,2 +1,50 @@
-let name = "Santhosh";
-console.log(name.length);
+// let name = "Santhosh";
+// console.log(name.length);
+// let name2 = "Santhosh          ";
+// console.log(name2.trim().length);
+let myHeroes = ["thor","spiderman"];
+let heroPower = {
+    thor:"hammer",
+    spiderman:"sling",
+    getspidermanPower:function(){
+        console.log(`Spiderman power is ${this.spiderman}`);
+    }
+}
+Object.prototype.Santhosh=function(){
+    console.log("Santhosh is present in all objects");
+}
+Array.prototype.heySanthosh = function(){
+    console.log("Santhosh says hello");
+}
+// heroPower.Santhosh()
+myHeroes.Santhosh()
+myHeroes.heySanthosh()
+// heroPower.heySanthosh()
+// inheritance
+const user={
+    name:"Santhosh",
+    email:"santhosh@gmail.com"
+}
+const teacher = {
+    makeVideo:true,
+}
+const teachingSupport = {
+    isAvailable:false,
+}
+const TASupport = {
+    makeAssignment:"JS Assignment",
+    fullTime :true,
+    // __proto__:teachingSupport,
+}
+// Teacher.__proto__=user;  
+// outdated structure above
+// modern apprach
+Object.setPrototypeOf(teachingSupport,teacher)
+let anotherUsername = "chai aur code    ";
+String.prototype.truelength = function(){
+    console.log(`${this}`);
+    console.log(`True length is: ${this.trim().length}`);
+}
+anotherUsername.truelength();
+"Santhosh".truelength();
+"icetea".truelength();
