d8a30b603ee2e3e7628e255a0d1b90bccfab9359
dev-Santhosh <130728015+dev-Santhosh@users.noreply.github.com>
2023-10-05 01:23:05 +0000

    commit

---
+++ 10_classes_oop/mathpi.js
@@ -0,0 +1,23 @@
+console.log(Math.PI);
+const descriptor=Object.getOwnPropertyDescriptor(Math,"PI");
+console.log(descriptor);
+const chai={
+    name:"chai",
+    price:25,
+    isavailable:true,
+    orderchai:function(){
+        console.log("Chai nahi bani");
+    }
+}
+console.log(chai);
+// console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+Object.defineProperty(chai,"name",{
+    writable:false,
+    enumerable:true,
+})
+console.log(Object.getOwnPropertyDescriptor(chai,"name"));
+for (let [key,value] of Object.entries(chai)) {
+    if(typeof value !== 'function'){
+        console.log(`${key}:${value}`);
+    }
+}
